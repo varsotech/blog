74b5f797ad1c8202419379f41d63e7e938eb7367
thenewl <thenewl@users.noreply.github.com>
2023-07-27 22:07:17 +0300

    awesome blog

---
+++ components/Comments.js
@@ -0,0 +1,55 @@
+import 'gitalk/dist/gitalk.css'
+import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
+import cn from 'classnames'
+import { fetchCusdisLang } from '@/lib/cusdisLang'
+import { useConfig } from '@/lib/config'
+
+const GitalkComponent = dynamic(
+  () => {
+    return import('gitalk/dist/gitalk-component')
+  },
+  { ssr: false }
+)
+const UtterancesComponent = dynamic(
+  () => {
+    return import('@/components/Utterances')
+  },
+  { ssr: false }
+)
+
+const Comments = ({ frontMatter }) => {
+  const router = useRouter()
+  const BLOG = useConfig()
+
+  const fullWidth = frontMatter.fullWidth ?? false
+
+  return (
+    <div
+      className={cn(
+        'px-4 font-medium text-gray-500 dark:text-gray-400 my-5',
+        fullWidth ? 'md:px-24' : 'mx-auto max-w-2xl',
+      )}
+    >
+      {BLOG.comment && BLOG.comment.provider === 'gitalk' && (
+        <GitalkComponent
+          options={{
+            id: frontMatter.id,
+            title: frontMatter.title,
+            clientID: BLOG.comment.gitalkConfig.clientID,
+            clientSecret: BLOG.comment.gitalkConfig.clientSecret,
+            repo: BLOG.comment.gitalkConfig.repo,
+            owner: BLOG.comment.gitalkConfig.owner,
+            admin: BLOG.comment.gitalkConfig.admin,
+            distractionFreeMode: BLOG.comment.gitalkConfig.distractionFreeMode
+          }}
+        />
+      )}
+      {BLOG.comment && BLOG.comment.provider === 'utterances' && (
+        <UtterancesComponent issueTerm={frontMatter.id} />
+      )}
+    </div>
+  )
+}
+
+export default Comments
