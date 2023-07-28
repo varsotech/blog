a64d34573074de10a5159c354897c432ecd7c35d
thenewl <thenewl@users.noreply.github.com>
2023-07-28 19:35:40 +0300

    made with

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,20 @@
 import { useConfig } from '@/lib/config'
+import { useState, useEffect } from 'react'
+
+const emojis = ["🏳️‍🌈", "☕️", "💜", "✨", "💻"]
+
 const Footer = ({ fullWidth }) => {
   const BLOG = useConfig()
 
   const d = new Date()
   const y = d.getFullYear()
   const from = +BLOG.since
+
+  const [emojiIndex, setEmojiIndex] = useState(false);
+  useEffect(() => {
+    setEmojiIndex(Math.floor(Math.random() * emojis.length));
+  }, [])
+
   return (
     <div
       className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
@@ -14,8 +24,13 @@ const Footer = ({ fullWidth }) => {
       <hr className="border-gray-200 dark:border-gray-600" />
       <div className="my-4 text-sm leading-6">
         <div className="flex align-baseline justify-between flex-wrap">
+          
           <p>
-            © {BLOG.title} {from === y || !from ? y : `${from} - ${y}`}
+            Made with {emojis[emojiIndex]} by L
+          </p>
+          <p style={{fontSize: 11}}>
+            <a href="https://github.com/varsotech/blog" style={{marginRight: 5}}>[ GitHub ]</a>
+            <a href="https://discord.gg/v27GZmcWZ8">[ Discord ]</a>
           </p>
         </div>
       </div>
